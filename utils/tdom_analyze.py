154fc645e51ca3434ca22ca2f4ef2a60c929378d
Enigmatisms <984041003@qq.com>
2023-03-25 17:19:10 +0800

    Time domain analysis enhanced.

--- utils/tdom_analyze.py
+++ utils/tdom_analyze.py
@@ -13,11 +13,55 @@ import matplotlib.pyplot as plt
 from scipy import io
 from utils.tools import folder_path
 from scene.opts import get_tdom_options
+from scipy.signal import find_peaks, peak_widths
 
 __all__ = ['time_domain_curve']
 
 colors = ("#DF7857", "#4E6E81", "#F99417")
 color_cnt = 0
+
+def lerp(start_pos, end_pos, data):
+    # linear interpolation for peak FWHM
+    start_sid = np.floor(start_pos).astype(np.int32)
+    start_eid = np.ceil(start_pos).astype(np.int32)
+    end_sid   = np.floor(end_pos).astype(np.int32)
+    end_eid   = np.ceil(end_pos).astype(np.int32)
+    
+    start_data = data[start_sid] * (start_pos - start_sid) + data[start_eid] * (start_eid - start_pos)
+    end_data   = data[end_sid] * (end_pos - end_sid) + data[end_eid] * (end_eid - end_pos)
+    return start_data, end_data
+
+def peak_analysis(curves: np.ndarray, ts: np.ndarray = None, prominence = 0.1, distance = 30, scaler = 1., unit = "s", show = False, sub_curve_avg = [0, 2]):
+    # analysis for curves: find peaks and calculate FWHM for each peak
+    if curves.ndim > 1:
+        result = np.zeros(curves.shape[-1])
+        for index in sub_curve_avg:
+            result += curves[index]
+        result /= len(sub_curve_avg)
+    else:
+        result = curves.copy()
+    peaks, _ = find_peaks(result, prominence = prominence, distance = distance)
+    # currently, the peak FWHF is calculated relatively (not absolutely)
+    # for curves that might be affected by ballistic photons, this FWHF method is not accurate
+    _, heights, left_ips, right_ips = peak_widths(result, peaks, rel_height = 0.5)
+    # You are going to need linear interpolation then
+    if ts is not None:
+        left_ips, right_ips = lerp(left_ips, right_ips, ts)
+    fwhm_width = right_ips - left_ips
+    print(f"{len(peaks)} detected, length:")
+    for i, width in enumerate(fwhm_width):
+        print(f"No.{i+1} peak, width = {width * scaler:.5f} {unit}")
+    if show == True:
+        ts = np.arange(result.shape[-1]) if ts is None else ts
+        plt.plot(ts, result, color = '#FF5533')
+        plt.scatter(ts, result, color = '#FF5533', s = 4)
+        plt.scatter(ts[peaks], result[peaks], s = 40, facecolors='none', edgecolors='b')
+        plt.grid(axis = 'both')
+        plt.title(f"Peak number: {len(peaks)}")
+        plt.xlabel(f"temporal progression, unit ({unit})")
+        plt.hlines(heights, left_ips, right_ips, color="#22BB22", linewidth = 2)
+        plt.show()
+    return peaks, heights, left_ips, right_ips
 
 def time_domain_curve(profiles: np.ndarray, window_mode = 'diag_tri', time_step = 1., sol = 1.0, name = "tdom-analysis", max_norm = True, viz = True):
     # transient profile shape (N, H, W, 3)
@@ -49,7 +93,7 @@ def time_domain_curve(profiles: np.ndarray, window_mode = 'diag_tri', time_step 
         visualize(results, ts, window_mode, max_time)
     return results, ts
 
-def visualize(results: np.ndarray, ts: np.ndarray, method: str, max_time: float, name = "AdaPT", show = True, whole_legend = 'whole image'):
+def visualize(results: np.ndarray, ts: np.ndarray, method: str, max_time: float, name = "AdaPT", show = True, whole_legend = 'whole image', extras = None):
     if method == "diag_tri":
         for i in range(3):
             plt.scatter(ts, results[i], s = 4, c = colors[i])
@@ -64,6 +108,19 @@ def visualize(results: np.ndarray, ts: np.ndarray, method: str, max_time: float,
         color_cnt += 1
         if show: plt.title(f"{name} window temporal analysis (whole image)")
     if show:
+        if extras is not None:
+            peaks     = extras['peaks']
+            heights   = extras['heights']
+            left_ips  = extras['left_ips']
+            right_ips = extras['right_ips']
+            scaler    = extras.get('scaler', 1.0)
+            unit      = extras.get('unit', 's')
+            plt.scatter(ts[peaks], results[peaks], s = 40, facecolors = 'none', edgecolors = 'b')
+            plt.hlines(heights, left_ips, right_ips, color = "#22BB22", linewidth = 2)
+            fwhm_width = right_ips - left_ips
+            print(f"{len(peaks)} detected, length:")
+            for i, width in enumerate(fwhm_width):
+                print(f"No.{i+1} peak, width = {width * scaler:.5f} {unit}")
         plt.legend()
         plt.grid(axis = 'both')
         plt.xlim((0, max_time))
@@ -101,7 +158,7 @@ def mat_data_reader(mat_file_path: str, var_name: str, mode = 'diag_tri', time_s
         visualize(results, ts, mode, max_time, name = "SPAD", show = show, whole_legend = whole_legend)
     return results, ts
 
-def sim_visualize(opts, whole_legend = 'whole image'):  
+def sim_visualize(opts, whole_legend = 'whole image', analyze_peak = True):  
     time_step = opts.sim_interval
     sol       = opts.sim_sol
     file_name = os.path.join(opts.sim_path, opts.sim_name)
@@ -113,7 +170,11 @@ def sim_visualize(opts, whole_legend = 'whole image'):
     transient_num = results.shape[-1]
     max_time = time_step * transient_num / sol
     ts = np.linspace(0., max_time, transient_num)
-    visualize(results, ts, opts.window_mode, max_time, whole_legend = whole_legend)
+    extras = None
+    if analyze_peak and results.ndim == 1:
+        peaks, heights, left_ips, right_ips = peak_analysis(results, ts)
+        extras = {'peaks': peaks, 'heights': heights, 'left_ips': left_ips, 'right_ips': right_ips}
+    visualize(results, ts, opts.window_mode, max_time, whole_legend = whole_legend, extras = extras)
 
 if __name__ == "__main__":
     opts = get_tdom_options()
