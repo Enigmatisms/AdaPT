fa8491dd8fd94df897cbd7c6e59920b02ad720f7
Qianyue He <984041003@qq.com>
2023-04-05 05:55:07 +0800

    Intial commit for spatial acceleration data structure.

---
+++ tracer/bvh.py
@@ -0,0 +1,8 @@
+"""
+    The bounding volume hierarchy (BVH) based on SAH methods 
+    The partition algorithm may have nothing to do with taichi
+    Therefore, I opt for C++ and pybind to do the job, making BVH construction faster
+    @author: Qianyue He
+    @date: 2023-4-5
+"""
+
