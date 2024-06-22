d418813428a431a3934539806c3888f49bf93177
Enigmatisms <984041003@qq.com>
2024-06-22 12:07:26 +0800

    Update volume methods

--- bxdf/volume.py
+++ bxdf/volume.py
@@ -8,7 +8,7 @@ import numpy as np
 import taichi as ti
 import xml.etree.ElementTree as xet
 
-from taichi.math import vec3
+from taichi.math import vec2, vec3, mat3
 
 from bxdf.phase import PhaseFunction
 from bxdf.medium import Medium_np
@@ -18,12 +18,16 @@ from sampler.general_sampling import random_rgb
 from rich.console import Console
 CONSOLE = Console(width = 128)
 
+vec3i = ti.types.vector(3, int)
+
 try:
     from vol_loader import vol_file_to_numpy
 except ImportError:
     CONSOLE.log("[red]:skeleton: Error [/red]: vol_loader is not found, possible reason:")
     CONSOLE.log(f"module not installed. Use 'python ./setup.py install --user' in {'./bxdf/'}")
     raise ImportError("vol_loader not found")
+
+__all__ = ["GridVolume_np", "GridVolume"]
 
 """ TODO: add transform parsing
 """
@@ -81,9 +85,68 @@ class GridVolume_np:
     def __repr__(self):
         return f"<Volume grid {self.type_name.upper()} with phase {self.phase_type}, albedo: {self.albedo},\n \
                 shape: (x = {self.xres}, y = {self.yres}, z = {self.zres}, c = {self.channel})>"
-
+    
 @ti.dataclass
 class GridVolume:
-    _type:  int
-    albedo: vec3            # scattering
+    """ Grid Volume Taichi End definition"""
+    _type:    int
+    """ grid volume type: 0 (none), 1 (mono-chromatic), 2 (RGB) """
+    albedo:   vec3            
+    """ scattering albedo """
+    inv_R:    mat3
+    """ Inverse rotation matrix (world frame) """
+    trans:    vec3
+    """ Translation vector (world frame) """
+    mini:     vec3
+    """ Min bound (AABB) minimum coordinate """
+    maxi:     vec3
+    """ Max bound (AABB) maximum coordinate """
+    min_idxs: vec3i            # min_indices
+    """ Minimum index, usually (0, 0, 0) """
+    max_idxs: vec3i            # max_indices
+    """ Maximum index for clamping, in case there's out-of-range access """
+    majorant: vec3            # max_values
+    """ Maximum value of sigma_t as majorant, for mono-chromatic """
+    ph:     PhaseFunction   # phase function
+    """ Phase function for scattering sampling """
+
+    @ti.func
+    def intersect_volume(self, ray_o: vec3, ray_d: vec3, max_t: float) -> vec2:
+        """ Get ray-volume intersection AABB near and far distance """
+        near_far = vec2([0, max_t])
+        inv_dir = 1.0 / ray_d
+
+        t1s = (self.mini - ray_o) * inv_dir
+        t2s = (self.maxi - ray_o) * inv_dir
+
+        tmin: vec3 = t1s.min(t2s)
+        tmax: vec3 = t1s.max(t2s)
+
+        near_far[0] = ti.max(ti.max(0, tmin[0]), ti.max(tmin[1], tmin[2]))
+        near_far[1] = ti.min(ti.min(max_t, tmax[0]), ti.min(tmax[1], tmax[2]))
+        return near_far
+
+    @ti.experimental.real_func
+    def transmittance(self, volume: ti.template, ray_o: vec3, ray_d: vec3, max_t: float) -> vec3:
+        transm = vec3([1, 1, 1])
+        near_far = self.intersect_volume(volume, ray_o, ray_d, max_t)
+        if near_far[0] >= near_far[1] or near_far[1] <= 0:
+            return transm
+        ray_o = self.inv_R @ (ray_o - self.trans)
+        ray_d = self.inv_R @ ray_d
+        if self._type:
+            if self._type == 1:     # single channel
+                return self.evalTrRatioTracking(volume, ray_o, ray_d, near_far)
+            else:
+                return self.evalTrRatioTracking3D(volume, ray_o, ray_d, near_far)
+        return transm
+    
+    @ti.func
+    def evalTrRatioTracking(self, volume: ti.template(), ray_ol: vec3, ray_dl: vec3, near_far: vec2) -> vec3:
+
+        pass
+
+    @ti.func
+    def evalTrRatioTracking3D(self, volume: ti.template(), ray_ol: vec3, ray_dl: vec3, near_far: vec2) -> vec3:
+        pass
     
